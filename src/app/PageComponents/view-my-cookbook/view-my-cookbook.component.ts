49ff35a43f494f1e59d25edd467552d788cc9034
JonKerr22 <jrkerr22@gmail.com>
2021-09-26 21:07:59 -0500

    adds new pages

---
+++ src/app/PageComponents/view-my-cookbook/view-my-cookbook.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-my-cookbook',
+  templateUrl: './view-my-cookbook.component.html',
+  styleUrls: ['./view-my-cookbook.component.css']
+})
+export class ViewMyCookbookComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,18 @@ import { HttpClientModule } from '@angular/common/http';
 import { RestService } from './Services/rest.service';
 import { HomePageComponent } from './PageComponents/home-page/home-page.component';
 import { AllCookbooksComponent } from './PageComponents/all-cookbooks/all-cookbooks.component';
+import { ViewMyCookbookComponent } from './PageComponents/view-my-cookbook/view-my-cookbook.component';
+import { LoginComponent } from './PageComponents/login/login.component';
+import { ViewRecipeComponent } from './PageComponents/view-recipe/view-recipe.component';
 
 @NgModule({
-  declarations: [
+  declarations: [ // TODO - don't just have everything declared at the app level, split it up some
     AppComponent,
     HomePageComponent,
-    AllCookbooksComponent
+    AllCookbooksComponent,
+    ViewMyCookbookComponent,
+    LoginComponent,
+    ViewRecipeComponent
   ],
   imports: [
     BrowserModule,
